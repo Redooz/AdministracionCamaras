a4aeffb36e74c25b5fd6e16663470aa8d7919c40
Nicolás Olmos <yohannicolas2003@gmail.com>
2022-08-13 16:23:55 -0500

    CamaraAnaloga class is now documented

--- src/modelo/CamaraAnaloga.java
+++ src/modelo/CamaraAnaloga.java
@@ -1,49 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 /**
- *
- * @author UD
+ * Clase abstracta que representa una camara analoga, tiene como atributos 
+ * id (String), marca (String), lente (String), precio (Double), rollo (String)
+ * y visor (String)
+ * @author Nicolás Olmos
+ * @author Daniel Garcia
  */
 public class CamaraAnaloga extends Camara{
     private String rollo, visor;
 
+    /**
+     * Constructor basico, crea una instancia de Camara analoga con sus atributos nulos
+     */
     public CamaraAnaloga() {
         this.rollo = "";
         this.visor = "";
     }
-
-    public CamaraAnaloga(String rollo, String visor) {
-        this.rollo = rollo;
-        this.visor = visor;
-    }
-
+    
+    /**
+     * Constructor parametrico, crea una instancia de Camara Analoga en base a los siguientes parametros
+     * @param rollo
+     * @param visor
+     * @param id
+     * @param marca
+     * @param lente
+     * @param precio
+     */
     public CamaraAnaloga(String rollo, String visor, String id, String marca, String lente, double precio) {
         super(id, marca, lente, precio);
         this.rollo = rollo;
         this.visor = visor;
     }
 
+    /**
+     * Metodo que calcula el descuento que tiene la camara analoga, el cual es del 10%
+     * @return
+     */
     @Override
     public double descuento(){
         return precio*0.1;
     }
     
+    /**
+     * Getter que retorna el valor de rollo
+     * @return String
+     */
     public String getRollo() {
         return rollo;
     }
 
+    /**
+     * Setter que modifica el valor de rollo
+     * @param rollo
+     */
     public void setRollo(String rollo) {
         this.rollo = rollo;
     }
 
+    /**
+     * Getter que retorna el valor de visor
+     * @return String
+     */
     public String getVisor() {
         return visor;
     }
 
+    /**
+     * Setter que modifica el valor de visor
+     * @param visor
+     */
     public void setVisor(String visor) {
         this.visor = visor;
     }
@@ -52,6 +78,11 @@ public class CamaraAnaloga extends Camara{
     public String toString() {
         return "Camara Analoga "+ super.toString()+ " Rollo: " + rollo + " Visor: " + visor;
     }
+    
+    /**
+     * Metodo que retorna los atributos de Camara Analoga en formato CSV
+     * @return String
+     */
     @Override
     public String datos(){
         return super.datos() + ";" + rollo + ";" + visor + ";--;--";
